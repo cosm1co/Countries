ed5dd7ae4d5c3bbbd2ff216d6f77667894928b49
cosm1co <federicofaraz@gmail.com>
2022-07-29 18:17:32 -0300

    port & node

--- index.js
+++ index.js
@@ -20,12 +20,13 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { getApiInfo } = require('./src/getApiInfo/getApiInfo.js');
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 
 conn.sync({ force: true }).then(getApiInfo, console.log('Countries loaded'))
 .then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`Listen on port ${PORT}`); // eslint-disable-line no-console
   });
 });
